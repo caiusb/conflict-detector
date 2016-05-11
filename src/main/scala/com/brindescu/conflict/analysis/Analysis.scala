b9d84f436d8bfa4307cde120ab21f4ab532478d6
Caius Brindescu <brindesc@oregonstate.edu>
2016-05-11 16:10:26 -0700

    Created a new Analysis class
    This contains everything needed to setup WALA correctly.
    
    And it actually works!

--- src/main/scala/com/brindescu/conflict/analysis/Analysis.scala
+++ src/main/scala/com/brindescu/conflict/analysis/Analysis.scala
@@ -1,8 +1,79 @@
 package com.brindescu.conflict.analysis
 
-object Analysis extends App {
+import com.brindescu.conflict.analysis.WALAConstants._
+import com.ibm.wala.ipa.callgraph.impl.ContextInsensitiveSelector
+import com.ibm.wala.ipa.callgraph.propagation.cfa.nCFAContextSelector
+import com.typesafe.config.{Config, ConfigFactory, ConfigList, ConfigValueFactory}
+import edu.illinois.wala.ipa.callgraph.FlexibleCallGraphBuilder
 
-	override def main(args: Array[String]) = {
+import scala.collection.JavaConversions._
 
+class Analysis {
+
+	private var config: Config = ConfigFactory.empty()
+
+	addJavaLibraries
+
+	def addJavaLibraries = {
+		val clazz = new Object().getClass
+		val classFile = clazz.getResource(clazz.getSimpleName + ".class").getFile
+		val rtPath: String = if (classFile.contains(".jar!"))
+			classFile.split("!")(0).split(":")(1)
+		else
+			classFile
+		config = config.withValue(JRE_LIBRARY, ConfigValueFactory.fromAnyRef(rtPath))
 	}
+
+	private def getConfigList(folder: String): ConfigList =
+		getConfigList(List(folder))
+
+	private def getConfigList(list: List[String]): ConfigList =
+		ConfigValueFactory.fromIterable(list)
+
+	private def getExisting(key: String): List[String] =
+		if (config.hasPath(key))
+			config.getValue(key).unwrapped().asInstanceOf[List[String]]
+		else
+			List()
+
+	def addSourceDependency(folder: String): Analysis = {
+		config = config.withValue(SOURCE_DEPENDENCY, getConfigList(getExisting(SOURCE_DEPENDENCY) :+ folder))
+		this
+	}
+
+	def addJarDependency(jarPath: String): Analysis = {
+		config = config.withValue(JAR_DEPENDENCY, getConfigList(getExisting(JAR_DEPENDENCY) :+ jarPath))
+		this
+	}
+
+	def addBinaryDepedency(classFolderPath: String): Analysis = {
+		config = config.withValue(BINARY_DEPENDENCY, getConfigList(getExisting(BINARY_DEPENDENCY) :+ classFolderPath))
+		this
+	}
+
+	def setEntryPoint(pattern: String): Analysis = {
+		config = config.withValue(ENTRY_POINT, ConfigValueFactory.fromAnyRef(pattern))
+		this
+	}
+
+	def setExclusion(pattern: String): Analysis = {
+		config = config.withValue(EXCLUSIONS, ConfigValueFactory.fromAnyRef("pattern"))
+		this
+	}
+
+	def getPointerAnalysis() = {
+		implicit val config = this.config
+		new FlexibleCallGraphBuilder() {
+			override def cs = new nCFAContextSelector(0, new ContextInsensitiveSelector())
+		}
+	}
+}
+
+private object WALAConstants {
+	val JRE_LIBRARY = "wala.jre-lib-path"
+	val SOURCE_DEPENDENCY = "wala.dependencies.source"
+	val BINARY_DEPENDENCY = "wala.dependencies.binary"
+	val JAR_DEPENDENCY = "wala.dependencies.jar"
+	val ENTRY_POINT = "wala.entry.signature-pattern"
+	val EXCLUSIONS = "wala.exclusions"
 }
--- src/test/scala/com/brindescu/conflict/analysis/SmokeTest.scala
+++ src/test/scala/com/brindescu/conflict/analysis/SmokeTest.scala
@@ -2,38 +2,27 @@ package com.brindescu.conflict.analysis
 
 import java.io.File
 
-import com.ibm.wala.ipa.callgraph.impl.ContextInsensitiveSelector
-import com.ibm.wala.ipa.callgraph.propagation.cfa.nCFAContextSelector
 import com.ibm.wala.util.graph.traverse.DFS
-import com.typesafe.config.{Config, ConfigFactory, ConfigValueFactory}
 import edu.illinois.wala.Facade._
-import edu.illinois.wala.ipa.callgraph.FlexibleCallGraphBuilder
 import edu.illinois.wala.ipa.callgraph.propagation.P
 import org.scalatest.{FlatSpec, Matchers}
+
 import scala.collection.JavaConversions._
 
 class SmokeTest extends FlatSpec with Matchers {
 
 	it should "correctly run WALA" in {
 
-		def createConfig(): Config = {
-			val clazz = new Object().getClass
-			val rtPath = clazz.getResource(clazz.getSimpleName + ".class").getFile.split("!")(0).split(":")(1)
-			ConfigFactory.empty().withValue("wala.jre-lib-path", ConfigValueFactory.fromAnyRef(rtPath))
-				.withValue("wala.entry.signature-pattern", ConfigValueFactory.fromAnyRef(".*Foo.*main.*"))
-				//		  .withValue("wala.dependencies-binary", ConfigValueFactory.fromAnyRef("target/scala-2.11/classes"))
-				.withValue("wala.dependencies.source", ConfigValueFactory.fromIterable(List(getResourceFile("/wala-smoke"))))
-				.withValue("wala.exclusions", ConfigValueFactory.fromAnyRef(""))
-		}
-
 		def getResourceFile(name: String): String =
 			new File(this.getClass.getResource(name).getFile).getAbsolutePath
 
-		implicit val config = createConfig()
+		val analysis = new Analysis()
+		  .setEntryPoint(".*Foo.*main.*")
+		  .addSourceDependency(getResourceFile("/wala-smoke"))
+		  .setExclusion("")
 
-		val pa = new FlexibleCallGraphBuilder() {
-			override def cs = new nCFAContextSelector(0, new ContextInsensitiveSelector())
-		}
+
+		val pa = analysis.getPointerAnalysis
 
 		import pa._
 
