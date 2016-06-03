7815c7dde55aca059ffe2ed975800d9e73965790
Caius Brindescu <brindesc@oregonstate.edu>
2016-06-02 22:03:12 -0700

    Extracted method.

--- src/test/scala/com/brindescu/conflict/analysis/MethodDUTest.scala
+++ src/test/scala/com/brindescu/conflict/analysis/MethodDUTest.scala
@@ -4,6 +4,12 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class MethodDUTest extends FlatSpec with Matchers with ResourceAccess {
 
+	private def getOverloadedAnalysis: Analysis =
+		new Analysis()
+			.setEntryPoint(".*OverloadedDefUse.*m.*")
+			.addSourceDependency(getResourceFile("/def-use"))
+			.setExclusion("")
+
 	it should "generate a correct DU graph" in {
 		val analysis = new Analysis()
 			.setEntryPoint(".*DefUseTest.*m.*")
@@ -19,10 +25,7 @@ class MethodDUTest extends FlatSpec with Matchers with ResourceAccess {
 	}
 
 	it should "generate multiple DU graphs for an overloaded method" in {
-		val analysis = new Analysis()
-		  .setEntryPoint(".*OverloadedDefUse.*m.*")
-		  .addSourceDependency(getResourceFile("/def-use"))
-		  .setExclusion("")
+		val analysis = getOverloadedAnalysis
 
 		val withVariables = analysis.getDUPathsForMethod("m")
 		withVariables should have size 2
@@ -31,10 +34,7 @@ class MethodDUTest extends FlatSpec with Matchers with ResourceAccess {
 	}
 
 	it should "generate uses for parameters" in {
-		val analysis = new Analysis()
-			.setEntryPoint(".*OverloadedDefUse.*m.*")
-			.addSourceDependency(getResourceFile("/def-use"))
-			.setExclusion("")
+		val analysis = getOverloadedAnalysis
 
 		val withVariables = analysis.getDUPathsForMethod("m")
 		withVariables.last.getUsesForVariable("a") should have size 1
